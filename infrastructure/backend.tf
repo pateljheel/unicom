5e7170ffa11a547741f9946321309aa8674f8970
Jheel Patel <git@jheelspace.com>
2025-02-22 12:42:03 -0500

    Add backend config

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,9 +1,9 @@
 # manage state in S3
 terraform {
   backend "s3" {
-    bucket         = "unifamily-tf-state"
+    bucket         = "s3-aws-project-22feb"
     key            = "terraform.tfstate"
-    region         = "us-east-2"
-    prefix         = "terraform/state"
+    region         = "us-east-1"
+    profile        = "unifamily"
   }
 }
