dc7dccc41aabd12d9f8af94ff8cf4e61f6f0a57d
Jheel Patel <git@jheelspace.com>
2025-02-22 11:09:00 -0500

    Update state bucket

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 # manage state in S3
 terraform {
   backend "s3" {
-    bucket         = "tf-state-<your-unique-bucket-name>"
+    bucket         = "unifamily-tf-state"
     key            = "terraform.tfstate"
     region         = "us-east-2"
     prefix         = "terraform/state"
