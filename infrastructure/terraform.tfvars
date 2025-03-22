7d22174a61b2ca34fe069171a12313a2c126c43d
Jheel Patel <jheel@noreply.com>
2025-03-22 10:24:31 -0400

    Add documentdb tf code

--- infrastructure/terraform.tfvars
+++ infrastructure/terraform.tfvars
@@ -3,7 +3,11 @@ app_name = "unifamily"
 app_environment = "dev"
 app_region = "us-east-2"
 
-
 # ec2 variables
 # ami_id = "ami-0fc82f4dabc05670b" # Amazon Linux 2023 AMI
 # instance_type = "t3.micro"
+
+# docdb variables
+db_username = "admin"
+db_password = "password1234" # Change this to a secure password
+db_instance_class = "db.t3.micro"
--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,59 +1,78 @@
 # Reference: https://spacelift.io/blog/terraform-aws-vpc
 # https://www.youtube.com/watch?v=pvhdT6Z92QU
 resource "aws_vpc" "main" {
- cidr_block = var.vpc_cidr
- 
- tags = {
-   Name = "Project VPC"
-   Environmnet = "Dev"
-   Terraform = "true"
- }
+  cidr_block = var.vpc_cidr
+
+  tags = merge(
+    var.additional_tags,
+    {
+      "Name"        = "${var.app_name}-${var.app_environment}-vpc",
+      "Environment" = var.app_environment,
+    }
+  )
 }
 
 resource "aws_subnet" "public_subnets" {
- count      = length(var.public_subnet_cidrs)
- vpc_id     = aws_vpc.main.id
- cidr_block = element(var.public_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
- tags = {
-   Name = "Public Subnet ${count.index + 1}"
- }
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = merge(
+    var.additional_tags,
+    {
+      "Name"        = "${var.app_name}-${var.app_environment}-public-subnet-${count.index}",
+      "Environment" = var.app_environment,
+    }
+  )
 }
- 
+
 resource "aws_subnet" "private_subnets" {
- count      = length(var.private_subnet_cidrs)
- vpc_id     = aws_vpc.main.id
- cidr_block = element(var.private_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
 
- tags = {
-   Name = "Private Subnet ${count.index + 1}"
- }
+  tags = merge(
+    var.additional_tags,
+    {
+      "Name"        = "${var.app_name}-${var.app_environment}-private-subnet-${count.index}",
+      "Environment" = var.app_environment,
+    }
+  )
 }
 
-resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.main.id
- 
- tags = {
-   Name = "Project VPC IG"
- }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.additional_tags,
+    {
+      "Name"        = "${var.app_name}-${var.app_environment}-igw",
+      "Environment" = var.app_environment,
+    }
+  )
 }
 
-resource "aws_route_table" "second_rt" {
- vpc_id = aws_vpc.main.id
- 
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.gw.id
- }
- 
- tags = {
-   Name = "2nd Route Table"
- }
+resource "aws_route_table" "public_subnets_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(
+    var.additional_tags,
+    {
+      "Name"        = "${var.app_name}-${var.app_environment}-public-subnets-rt",
+      "Environment" = var.app_environment,
+    }
+  )
 }
 
 resource "aws_route_table_association" "public_subnet_asso" {
- count = length(var.public_subnet_cidrs)
- subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
- route_table_id = aws_route_table.second_rt.id
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_subnets_rt.id
 }
